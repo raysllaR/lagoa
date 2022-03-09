c88ece0f81a478eab7d1b78bc5ec2746ba0f9b21
raysllaR <rayslla.rodrigues.ferreira@gmail.com>
2022-03-09 09:00:54 -0300

    Adicionando quantidade ao carrinho

--- src/components/corpoComponents/Carrinho.js
+++ src/components/corpoComponents/Carrinho.js
@@ -4,11 +4,10 @@ import './styles/Carrinho.css';
 
 const Carrinho = ({itensCarrinho, itens, date, groups, idGrupoSelecionado, setIdGrupoSelecionado, buttonCompraText, setButtonCompraText}) => {
   let [ yy, mm, dd ] = date.split('-'); 
-  console.log(groups);
   mm = new Date(date).toLocaleString('default', {month: 'long'});
 
-  const next = () => {
-      console.log(idGrupoSelecionado);
+  const next = (event) => {
+    event.stopPropagation()
       let indexOf;
       groups.findIndex((grupo, index) => {
         console.log(index); 
@@ -26,15 +25,49 @@ const Carrinho = ({itensCarrinho, itens, date, groups, idGrupoSelecionado, setId
   React.useEffect(()=>{
     console.log("lgth ", Object.keys(itensCarrinho).length === 0)
     if(Object.keys(itensCarrinho).length === 0){
-      document.querySelector('.btn-carrinho-next').setAttribute("disabled", "disabled")
+      document.querySelectorAll('.btn-carrinho-next').forEach(item => item.setAttribute("disabled", "disabled"));
       
     } else {
-      document.querySelector('.btn-carrinho-next').removeAttribute('disabled')
+      document.querySelectorAll('.btn-carrinho-next').forEach(item => item.removeAttribute('disabled'))
     }
-  }, [itensCarrinho])
+
+    document.querySelectorAll('.contador-carrinho').forEach(contador => {
+      const keys = Object.keys(itensCarrinho);
+
+      contador.innerText = keys.reduce( (soma, key) => { return soma + itensCarrinho[key].quantidade }, 0);
+    });
+  }, [itensCarrinho]);
+
+  const openCarrinho = ({target}) => {
+    const divCarrinho = document.querySelector('.container-carrinho');
+    const divListaProdutosCarrinho = document.querySelector('.lista-produtos-carrinho');
+    const divContainerBtnCarrinho = document.querySelector('.container-btn-carrinho');
+
+    if(divCarrinho.classList.contains('fechado')){
+      divCarrinho.classList.remove('fechado');
+      divCarrinho.classList.add('aberto'); 
+      divListaProdutosCarrinho.classList.add('aberto');
+      if(Object.keys(itensCarrinho).length != 0) {
+        divContainerBtnCarrinho.classList.add('aberto-com-produtos')
+        divContainerBtnCarrinho.classList.remove('fechado')
+      } else{
+        divContainerBtnCarrinho.style.display = 'none'
+      }
+    } else {
+      if(divContainerBtnCarrinho.classList.contains('aberto-com-produtos')){
+        divContainerBtnCarrinho.classList.remove('aberto-com-produtos');
+        divContainerBtnCarrinho.classList.add('fechado')
+      } else{
+        divContainerBtnCarrinho.style.display = 'flex'
+      }
+      divListaProdutosCarrinho.classList.remove('aberto');
+      divCarrinho.classList.remove('aberto');
+      divCarrinho.classList.add('fechado');
+    }
+  }
 
   return (
-    <div className="container-carrinho fechado">
+    <div className="container-carrinho fechado" onClick={openCarrinho}>
       <div className="container-details-carrinho">
         <div className="details-carrinho">
           <div className="container-data-carrinho">
@@ -70,7 +103,7 @@ const Carrinho = ({itensCarrinho, itens, date, groups, idGrupoSelecionado, setId
           <div className="lista-produtos-carrinho fechado"><span className="nenhum-produto">Nenhum produto adicionado ao carrinho</span></div>
         </div>
         <div className="container-btn-carrinho fechado">
-            <Link to='/' className="btn-carrinho btn-carrinho-buy-other-day">Comprar para outro dia
+            <Link to='/' className="btn-carrinho btn-carrinho-buy-other-day" onClick={(event) => event.stopPropagation()}>Comprar para outro dia
               <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" style={{width: '17px', height: '17px', marginRight: '5px'}} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
             </Link>
           <button className="btn-carrinho btn-carrinho-next" onClick={next} >{buttonCompraText}
